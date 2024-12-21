9cc5952cecca6f02d22c2111414f9f12158ed24a
kurtYmd <kurtimdmitruk@gmail.com>
2024-12-21 11:09:18 +0100

    Initial Commit

---
+++ Gain/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gain
+//
+//  Created by Bohdan Dmytruk on 21/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gain/GainApp.swift
@@ -0,0 +1,17 @@
+//
+//  GainApp.swift
+//  Gain
+//
+//  Created by Bohdan Dmytruk on 21/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct GainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
